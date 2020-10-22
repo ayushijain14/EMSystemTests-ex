06af562c6763d7447ad1ab9ce24b80d84161ff2e
ayushijain14 <ayushiashu14@gmail.com>
2020-10-22 16:43:36 +0530

    checked

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -54,7 +54,7 @@ public class ExpenseManagerSystemTest {
 		
         if(result.getStatus() == ITestResult.FAILURE) {
 			//You need to give your JIRA Username and Password in below line
-        	BasicCredentials cred = new BasicCredentials("ayushiashu14","@yu$hi@1234 ");
+        	BasicCredentials cred = new BasicCredentials("ayushiashu14","@yu$hi@1234");
 			
 			//You need to update your JIRA machine IP address with port 8080 in below line
         	JiraClient jira = new JiraClient("http://54.89.194.44:8080/", cred);
